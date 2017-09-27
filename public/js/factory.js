2ffd57ed60b1bfb4207119b718815a876e5464f9
Josue Plaza <josue@parlevelsystems.com>
2017-09-27 00:01:31 -0500

    - multiple stream retrieval iteration

--- public/js/factory.js
+++ public/js/factory.js
@@ -46,6 +46,13 @@ function mainFactory($http, $q, $log) {
         // var players = foreach loop to concat &user_login=
         
         // var list = "?";
+        var list = '?';
+        for (var i = 0; i < players.length; i++) {
+            list = list + '&user_login=' + players[i].stream_username;
+            // TODO cycle and send the request every 100 iterations.
+            // This logic should be a promise because this call can have the potential to clal multiple times
+        }
+        console.log(list);
        
         
         
