ce7cdf26627978df52f61a01a72aab28f8711f62
Josue Plaza <josue@parlevelsystems.com>
2017-10-03 09:57:35 -0500

    added looping structure to twitch get players logic

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,44 +16,41 @@ class MainController extends Controller
         * a variety of different parameters
         */
         $players = Player::with('team')->get();
-        
-        // TODO DYNAMIC LIST
-        // $list = '?';
-        // for ($i = 0; $i < players.length; i++) {
-        //     list = list + '&user_login=' + players[i].twitch_username;
-        //     // TODO cycle and send the request every 100 iterations.
-        //     // This logic should be a promise because this call can have the potential to clal multiple times
-        // }
-        // console.log(list);
-        
-        
-        $curl = curl_init();
-
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://api.twitch.tv/helix/streams?user_login=tsm_hauntzer&user_login=tsm_svenkeren&user_login=tsm_bjergsen&user_login=tsm_doublelift&user_login=tsm_biofrost&user_login=akaadian",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "cache-control: no-cache",
-                "client-id: yzim3jwu69i42xbkivk5lj01jbo1pw"
-            ],
-        ]);
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-          Log::info("cURL Error #:" . $err);
-      } else {
-          Log::info($response);
-      }
-      
-      return response()->json(['players' => $players, 'key' => env('TWITCH_API_KEY')]);
-  }
+        $list = '?';
+        $i = 0;
+        $activeStreams = [];
+        foreach($players as $player) {
+            $list .= 'user_id=' . $player->twitch_user_id . '&';
+            $i++;
+            if ($i == 100) {
+                $curl = curl_init();
+                curl_setopt_array($curl, [
+                    CURLOPT_URL => "https://api.twitch.tv/helix/streams" . $list,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_HTTPHEADER => [
+                        "cache-control: no-cache",
+                        "client-id: yzim3jwu69i42xbkivk5lj01jbo1pw"
+                    ],
+                ]);
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+                if ($err) {
+                    Log::info("cURL Error #:" . $err);
+                } else {
+                    // Create array of current streams
+                    // Add to $activeStreams;
+                    Log::info($response);
+                }
+                $i = 0;
+                $list = '?';
+            }
+        }
+        return response()->json(['players' => $players, 'activeStreams' => $activeStreams, 'key' => env('TWITCH_API_KEY')]); 
+    }
 }
