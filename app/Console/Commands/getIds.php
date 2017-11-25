f1b4c2e04da15337c862ae85841ad7709a1b7927
Josue Plaza <josue@parlevelsystems.com>
2017-11-25 13:14:29 -0600

    added team colors to player-box, various CSS updates

--- app/Console/Commands/getIds.php
+++ app/Console/Commands/getIds.php
@@ -39,56 +39,60 @@ class getIds extends Command
      */
     public function handle()
     {
-        $players = Player::where('twitch_user_id', null)->get();
+        $players = Player::where('twitch_user_id', null)->where('twitch_username', '!=', 0)->get();
         $count = $players->count();
         
-        $this->info('Getting Twitch User IDs for ' . $count . ' players');
-        $list = '?';
-        
-        foreach($players as $player) {
-            $list .= 'login=' . $player->twitch_username . '&';
-        }
-        Log::info($list);
-        
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.twitch.tv/helix/users" . $list,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_HTTPHEADER => array(
-            "cache-control: no-cache",
-            "client-id: yzim3jwu69i42xbkivk5lj01jbo1pw"
-          ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        if ($err) {
-          $this->error("cURL Error #:" . $err);
-        }
-        $response = json_decode($response, true);
-        $twitchCount = count($response['data']);
-                
-        $this->info('Retrieved User IDs for ' . $twitchCount . ' players');
-        
-        foreach($response['data'] as $user) {
-            $player = Player::where('twitch_username', $user['login'])->first();
-            $player->twitch_user_id = $user['id'];
-            $player->save();
-        }
-        
-        if ($twitchCount != $count) {
-            $difference = $count - $twitchCount;
-            $this->error($difference . ' player ID(s) could not be found. Here they are:');
-            $players = Player::where('twitch_user_id', null)->get();
+        if ($count > 0) {
+            $this->info('Getting Twitch User IDs for ' . $count . ' players');
+            $list = '?';
+            
             foreach($players as $player) {
-                $this->line($player->handle . ': ' . $player->twitch_username);
+                $list .= 'login=' . $player->twitch_username . '&';
             }
+            Log::info($list);
+            
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => "https://api.twitch.tv/helix/users" . $list,
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 30,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "GET",
+              CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "client-id: yzim3jwu69i42xbkivk5lj01jbo1pw"
+              ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            if ($err) {
+              $this->error("cURL Error #:" . $err);
+            }
+            $response = json_decode($response, true);
+            $twitchCount = count($response['data']);
+                    
+            $this->info('Retrieved User IDs for ' . $twitchCount . ' players');
+            
+            foreach($response['data'] as $user) {
+                $player = Player::where('twitch_username', $user['login'])->first();
+                $player->twitch_user_id = $user['id'];
+                $player->save();
+            }
+            
+            if ($twitchCount != $count) {
+                $difference = $count - $twitchCount;
+                $this->error($difference . ' player ID(s) could not be found. Here they are:');
+                $players = Player::where('twitch_user_id', null)->get();
+                foreach($players as $player) {
+                    $this->line($player->handle . ': ' . $player->twitch_username);
+                }
+            }
+        } else {
+            $this->info('No ids to grab.');
         }
     }
 }
